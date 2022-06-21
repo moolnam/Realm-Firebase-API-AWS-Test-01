63d1028c9833945a72cbfbed8a16dd022a23b32a
moolnam <moolnam>
2022-06-21 21:19:36 +0900

    Initial Commit

---
+++ Realm-Test02/Realm_Test02App.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_Test02App.swift
+//  Realm-Test02
+//
+//  Created by KimJongHee on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Realm_Test02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
